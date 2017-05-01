36d889338777744874da80b5673b884b2e2e5395
Dan Schultzer <dan@dreamconception.com>
2017-05-01 08:06:21 -0700

    Make travis work

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,3 +3,4 @@
 // Enable Composer autoloader
 $autoloader = require __DIR__ . '/../vendor/autoload.php';
 $autoloader->addPsr4('MonitGraph\Tests\\', __DIR__);
+$autoloader->addPsr4('MonitGraph\Tests\Web\\', __DIR__ . '/web');
